67732f2c207213152f7eee7f2a9f11f4a882dd41
DavisDavisDavis <olizoooom@gmail.com>
2022-08-24 11:16:09 +0200

    Initial unity

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,10 +4,21 @@ import { useEffect } from "react";
 import logo from "./assets/images/logo.svg";
 import orange_form from "./assets/images/orange-form.svg";
 import pink_form from "./assets/images/pink-form.svg";
+<<<<<<< Updated upstream
 import About from "./components/about/about";
 
+=======
+import { Unity, useUnityContext } from "react-unity-webgl";
+>>>>>>> Stashed changes
 
 function App() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "/build/TheoryOfChangeBuilds.loader.loader.js",
+    dataUrl: "/build/TheoryOfChangeBuilds.loader.data",
+    frameworkUrl: "/build/TheoryOfChangeBuilds.loader.framework.js",
+    codeUrl: "/build/TheoryOfChangeBuilds.loader.wasm",
+  });
+
   return (
     <>
       <header>
@@ -22,9 +33,16 @@ function App() {
         <div className="unity"></div>
         <img className="pink-form" src={pink_form} />
       </main>
+<<<<<<< Updated upstream
 
       <About />
 
+=======
+      <Unity
+        unityProvider={unityProvider}
+        style={{ width: 800, height: 600 }}
+      />
+>>>>>>> Stashed changes
     </>
   );
 
