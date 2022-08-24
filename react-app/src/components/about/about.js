e15cb6e604bfde3be2416dbe9e2bb50dec60d48f
Amanda Karlsson Printz <90208099+amandaprintz@users.noreply.github.com>
2022-08-24 13:18:39 +0200

    added socials

--- react-app/src/components/about/about.js
+++ react-app/src/components/about/about.js
@@ -8,7 +8,6 @@ export default function About () {
   
   return (
 
-
     <div className="container">
       <div className="item">
       <div class="photo">
@@ -22,50 +21,66 @@ export default function About () {
      <img src={linkedin} />
       </div>
    </div>
-
-
-
-
-      <div className="item">
+   <div className="item">
       <div class="photo">
-    <img src={doggie} />
-    <div class="info"> 
-    <h2>Namn</h2>
-    <h3>Titel</h3></div>
-   </div>
+      <img src={doggie} />
+      <div class="info"> 
+      <h2>Namn</h2>
+      <h3>Titel</h3></div>
       </div>
-      <div className="item">
+     <div class="socials"> 
+     <img src={mail} />
+     <img src={linkedin} />
+      </div>
+   </div>
+   <div className="item">
       <div class="photo">
-    <img src={doggie} />
-    <div class="info"> 
-    <h2>Namn</h2>
-    <h3>Titel</h3></div>
-   </div>
+      <img src={doggie} />
+      <div class="info"> 
+      <h2>Namn</h2>
+      <h3>Titel</h3></div>
       </div>
-      <div className="item">
+     <div class="socials"> 
+     <img src={mail} />
+     <img src={linkedin} />
+      </div>
+   </div>
+   <div className="item">
       <div class="photo">
-    <img src={doggie} />
-    <div class="info"> 
-    <h2>Namn</h2>
-    <h3>Titel</h3></div>
-   </div>
+      <img src={doggie} />
+      <div class="info"> 
+      <h2>Namn</h2>
+      <h3>Titel</h3></div>
       </div>
-      <div className="item">
+     <div class="socials"> 
+     <img src={mail} />
+     <img src={linkedin} />
+      </div>
+   </div>
+   <div className="item">
       <div class="photo">
-    <img src={doggie} />
-    <div class="info"> 
-    <h2>Namn</h2>
-    <h3>Titel</h3></div>
-   </div>
+      <img src={doggie} />
+      <div class="info"> 
+      <h2>Namn</h2>
+      <h3>Titel</h3></div>
       </div>
-      <div className="item">
+     <div class="socials"> 
+     <img src={mail} />
+     <img src={linkedin} />
+      </div>
+   </div>
+   <div className="item">
       <div class="photo">
-    <img src={doggie} />
-    <div class="info"> 
-    <h2>Namn</h2>
-    <h3>Titel</h3></div>
-   </div>
+      <img src={doggie} />
+      <div class="info"> 
+      <h2>Namn</h2>
+      <h3>Titel</h3></div>
       </div>
+     <div class="socials"> 
+     <img src={mail} />
+     <img src={linkedin} />
+      </div>
+   </div>
     </div>
   )
 }
