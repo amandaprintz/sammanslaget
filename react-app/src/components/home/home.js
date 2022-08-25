12e49e8746d5a9278a666277e261c79ef7b1148f
DavisDavisDavis <olizoooom@gmail.com>
2022-08-25 13:47:14 +0200

    Tshirt frontpage

--- react-app/src/components/home/home.js
+++ react-app/src/components/home/home.js
@@ -1,20 +1,15 @@
-import React from 'react';
+import React from "react";
 import "./home.css";
-
+import tshirt from "../../assets/images/tshirt.svg";
 
 export default function Project() {
-    return (
-
-/*      <Header />
- */
-        <div className="project-container">
-        <h1>Titel på projektet</h1>
-        <h5>Här är introtexten till </h5>
-
-
-</div>
-
-
-
-    )
+  return (
+    /*      <Header />
+     */
+    <div className="project-container">
+      <img className="tshirt" src={tshirt} />
+      <h1>Titel på projektet</h1>
+      <h5>Här är introtexten till </h5>
+    </div>
+  );
 }
